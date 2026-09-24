41f0ddcffbaedbb443182d1517683f77a853f962
Victor Kovalchuk <vict02r05@gmail.com>
2026-09-24 22:36:15 +0300

    working with fragments

--- section-9-diving-deeper-into-components/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ section-9-diving-deeper-into-components/cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,17 +1,15 @@
 <template>
-  <div>
-    <h2>Manage Goals</h2>
-    <input type="text" ref="goal">
-    <button @click="setGoal">Set Goal</button>
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal">
+  <button @click="setGoal">Set Goal</button>
 
-    <teleport to="body">
-      <ErrorAlert v-if="inputIsInvalid">
-        <h2>Input is invalid!</h2>
-        <p>Please enter at least a few characters...</p>
-        <button @click="confirmError">Okay</button>
-      </ErrorAlert>
-    </teleport>
-  </div>
+  <teleport to="body">
+    <ErrorAlert v-if="inputIsInvalid">
+      <h2>Input is invalid!</h2>
+      <p>Please enter at least a few characters...</p>
+      <button @click="confirmError">Okay</button>
+    </ErrorAlert>
+  </teleport>
 </template>
 
 <script>
