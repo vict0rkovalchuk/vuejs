300d9bce2c0029090ea8b54222247bc1d5f670ed
Victor Kovalchuk <vict02r05@gmail.com>
2026-09-24 22:01:05 +0300

    dynamic components

--- section-9-diving-deeper-into-components/cmp-adv-01-starting-setup/src/App.vue
+++ section-9-diving-deeper-into-components/cmp-adv-01-starting-setup/src/App.vue
@@ -1,35 +1,31 @@
 <template>
   <div>
     <TheHeader />
-    <BadgeList />
-    <UserInfo 
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    />
-    <CourseGoals #default="slotProps">
-      <!-- <template #default="slotProps"> -->
-      <h2>{{slotProps.item}}</h2>
-      <!-- </template> -->
-    </CourseGoals>
+    <button @click="setSelectedComponent('ActiveGoals')">Active Goals</button>
+    <button @click="setSelectedComponent('ManageGoals')">Manage Goals</button>
+    <!-- <ActiveGoals v-if="selectedComponent === 'ActiveGoals'"/> -->
+    <!-- <ManageGoals v-if="selectedComponent === 'ManageGoals'"/> -->
+    <component :is="selectedComponent"></component>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+// import CourseGoals from './components/CourseGoals.vue';
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 
 export default {
   components: { 
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals
+    ActiveGoals,
+    ManageGoals
   },
   data() {
     return {
+      selectedComponent: 'ActiveGoals',
       activeUser: {
         name: 'Maximilian Schwarzmüller',
         description: 'Site owner and admin',
@@ -37,6 +33,11 @@ export default {
       },
     };
   },
+  methods: {
+    setSelectedComponent(component) {
+      this.selectedComponent = component;
+    }
+  }
 };
 </script>
 
