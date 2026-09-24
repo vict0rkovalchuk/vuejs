f9cfee129d8d0c2e6108dd9b63331a2b2a3a93a4
Victor Kovalchuk <vict02r05@gmail.com>
2026-09-24 23:10:50 +0300

    first components and props

--- prj-cmp-01-starting-setup/src/App.vue
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <ul></ul>
+  <ul>
+    <LearningItem
+      v-for="resource in storedResources" 
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    />
+  </ul>
 </template>
 
 <script>
+import LearningItem from './components/learning-resources/LearningItem.vue';
+
 export default {
+  components: {
+    LearningItem
+  },
   data() {
     return {
       storedResources: [
